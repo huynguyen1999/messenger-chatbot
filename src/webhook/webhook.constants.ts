24918638dc4d9ad14e74af6ffba324911b0814d3
Nguyen Tran Cong Huy <conghuy.nguyen@finviet.corp>
2022-08-10 15:49:11 +0700

    commit

---
+++ src/webhook/webhook.constants.ts
@@ -0,0 +1,14 @@
+export const DEFINED_INTENTS = {
+  ORDER_REQUEST: 'order_request',
+  ORDER_PAYMENT: 'order_payment',
+  GOODBYE: 'goodbye',
+  GREET: 'greet',
+  HELP: 'help',
+};
+
+export const DEFINED_ENTITIES = {
+  PRODUCT: 'product',
+  ADDRESS: 'address',
+};
+
+export const CONFIDENCE_THRESHOLD = 0.5;
