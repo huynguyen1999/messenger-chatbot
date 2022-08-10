7b0cf4e30a0c15a024abc4c691fcc903e7f8205c
Nguyen Tran Cong Huy <conghuy.nguyen@finviet.corp>
2022-08-10 09:59:22 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {configuration} from './configuration';
+require('dotenv');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configs = configuration();
-  const PORT = configs.app.port || 3000;
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT, ()=>console.log(`App is listening at port ${PORT}...`));
 }
 bootstrap();
